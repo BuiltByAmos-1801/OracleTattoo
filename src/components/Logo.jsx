7769fa533dca0dda1b441e25d761868be8e9f763
BuiltByAmos-1801 <builtbiamos@gmail.com>
2026-06-02 22:07:18 +0530

    Added Code In Files

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logoSrc from "../images/Oracle_Tattoo_Logo.PNG";
 
-export default function Logo({ className = "" }) {
+export default function Logo({ className = "", invert = false }) {
   return (
     <Link to="/" className={`inline-block shrink-0 ${className}`} aria-label="Oracle Tattoo home">
-      <img src={logoSrc} alt="Oracle Tattoo" className="w-[80px] h-auto object-contain object-left invert" width={150} height={38} />
+      <img src={logoSrc} alt="Oracle Tattoo" className={`w-[116px] h-auto object-contain object-left sm:w-[150px] ${invert ? "invert" : ""}`} width={150} height={38} />
     </Link>
   );
 }
