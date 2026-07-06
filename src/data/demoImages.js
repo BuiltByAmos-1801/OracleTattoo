4eb542fc15e59fd88a6e8dd5f149e29a45b1c0d4
BuiltByAmos-1801 <builtbiamos@gmail.com>
2026-07-06 20:49:31 +0530

    Auto update: 2026-07-06 20:49:30

--- src/data/demoImages.js
+++ src/data/demoImages.js
@@ -8,7 +8,6 @@ import featured04 from "../images/Screenshot 2026-06-06 235906.png";
 import featured05 from "../images/Screenshot 2026-06-06 235950.png";
 import featured06 from "../images/Screenshot 2026-06-07 000014.png";
 import featured07 from "../images/Screenshot 2026-06-07 000135.png";
-import artistNew from "../images/artist_new.jpg";
 
 const unsplash = (id, width = 1200) =>
   `https://images.unsplash.com/${id}?auto=format&fit=crop&w=${width}&q=88`;
@@ -42,8 +41,8 @@ export const demoImages = {
     unsplash("photo-1598371839696-5c5bb00bdc28", 900),
     unsplash("photo-1542727365-19732a80dcfd", 900),
     unsplash("photo-1562962230-16e4623d36e6", 900),
-    unsplash("photo-1542727365-19732a80dcfd", 900)
-    , artistNew
+    unsplash("photo-1542727365-19732a80dcfd", 900),
+    unsplash("photo-1488426862026-3ee34a7d66df", 900)
   ],
   blogHero: pexels("36170868", 1800),
   blog: [
